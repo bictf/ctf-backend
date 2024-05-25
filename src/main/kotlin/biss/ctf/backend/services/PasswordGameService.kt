d7601c0b20a2ca8bfdf27fc4af26f973528dfcd8
rhron255 <ronhasleton@gmail.com>
2024-05-26 00:06:11 +0300

    # Improvements For BISS-YH ## Password Game - Added interface for game levels. - Added length password requirement. - Added password game level service. - Added controller for requesting levels.

---
+++ src/main/kotlin/biss/ctf/backend/services/PasswordGameService.kt
@@ -0,0 +1,21 @@
+package biss.ctf.backend.services
+
+import biss.ctf.backend.services.passwordlevels.PasswordGameLevel
+import org.springframework.stereotype.Service
+
+@Service
+class PasswordGameService(
+    private val passwordLevels: List<PasswordGameLevel>
+) {
+    private val userLevelMap = HashMap<String, List<PasswordGameLevel>>()
+
+    fun getUserLevels(uuid: String): List<PasswordGameLevel> {
+        return userLevelMap.computeIfAbsent(uuid) { _ -> passwordLevels.shuffled() }
+    }
+
+    fun getNextUserLevel(uuid: String, currentPassword: String): PasswordGameLevel? {
+        return runCatching {  getUserLevels(uuid).first { !it.doesAnswerLevel(currentPassword) } }.getOrNull()
+    }
+
+    // TODO implement game ending logic. **IMPORTANT** the above wil return null also if the answer is correct for all the levels.
+}
