90ae60eee7f62e20e822b0172d587d8f13a5ec35
RedHeadKid <galyurman111@gmail.com>
2024-06-03 20:57:17 +0300

    done

---
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/HeartachePasswordLevel.kt
@@ -0,0 +1,21 @@
+package biss.ctf.backend.services.passwordlevels
+
+import org.springframework.stereotype.Component
+
+@Component
+class HeartachePasswordLevel: PasswordGameLevel {
+    companion object {
+        private const val KEY_WORD = "heartache"
+    }
+    override fun getLevelDescription(): String {
+        return "Password must contain the 44th word of our favorite song!"
+    }
+
+    override fun getLevelHint(): String {
+        return "Believe, when I say..."
+    }
+
+    override fun doesAnswerLevel(password: String): Boolean {
+        return password.lowercase().contains(KEY_WORD)
+    }
+}
