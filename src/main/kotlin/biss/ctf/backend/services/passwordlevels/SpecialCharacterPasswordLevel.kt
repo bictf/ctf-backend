f5b611ad28296add0448f6574df5a09ddf57c0eb
rhron255 <142088780+rhron255@users.noreply.github.com>
2024-06-15 17:55:47 +0300

    password must include one or more special characters (#48)

---
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/SpecialCharacterPasswordLevel.kt
@@ -0,0 +1,29 @@
+package biss.ctf.backend.services.passwordlevels
+
+import org.springframework.stereotype.Component
+
+@Component
+class SpecialCharacterPasswordLevel: PasswordGameLevel {
+    companion object {
+        private val specialCharacters = arrayOf(
+            "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+            "[", "]", "{", "}", "|", "\\", ":", ";", "\"", "'", "<", ">", ",", ".", "?", "/"
+        )
+    }
+    override fun getLevelDescription(): String {
+        return "Password must include special characters"
+    }
+
+    override fun getLevelHint(): String {
+        return "\$p3c14L"
+    }
+
+    override fun doesAnswerLevel(password: String): Boolean {
+        for (character in specialCharacters) {
+            if (password.contains(character)) {
+                return true
+            }
+        }
+        return false
+    }
+}
