31a7f3a43e6977fce6e574497257df39db4a8ad3
unknown <lhdd7950@gmail.com>
2024-06-24 20:07:18 +0300

    added backend to the sponsor level

---
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/IncludeSponsorPasswordLevel.kts
@@ -0,0 +1,21 @@
+package biss.ctf.backend.services.passwordlevels
+
+import org.springframework.stereotype.Component
+
+@Component
+class IncludeSponsorPasswordLevel: PasswordGameLevel {
+    companion object {
+        private const val SPONSOR = "olga"
+    }
+    override fun getLevelDescription(): String {
+        return "Password must include the sponsor of the biss"
+    }
+
+    override fun getLevelHint(): String {
+        return "Is she the owner? or just a random russian woman..."
+    }
+
+    override fun doesAnswerLevel(password: String): Boolean {
+        return password.lowercase().contains(SPONSOR)
+    }
+}
