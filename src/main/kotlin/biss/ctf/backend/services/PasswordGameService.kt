e89100ba795bc9611fdbbb761a7f74e7c858bdc4
rhron255 <ronhasleton@gmail.com>
2024-06-27 18:38:02 +0300

    Removed unused methods.

--- src/main/kotlin/biss/ctf/backend/services/PasswordGameService.kt
+++ src/main/kotlin/biss/ctf/backend/services/PasswordGameService.kt
@@ -7,19 +7,11 @@ import org.springframework.stereotype.Service
 class PasswordGameService(
     private val passwordLevels: List<PasswordGameLevel>
 ) {
-    private val userLevelMap = HashMap<String, List<PasswordGameLevel>>()
-
-    fun getUserLevels(uuid: String): List<PasswordGameLevel> {
-        return userLevelMap.computeIfAbsent(uuid) { _ -> passwordLevels.shuffled() }
-    }
-
-    fun getNextUserLevel(uuid: String, currentPassword: String): PasswordGameLevel? {
-        return runCatching {  getUserLevels(uuid).first { !it.doesAnswerLevel(currentPassword) } }.getOrNull()
-    }
-
     fun getAllLevels() = passwordLevels
 
     fun getNextLevel(currentPassword: String) = getAllLevels().first { !it.doesAnswerLevel(currentPassword) }
+
+    fun getLevelCount() = passwordLevels.size
 
     // TODO implement game ending logic. **IMPORTANT** the above wil return null also if the answer is correct for all the levels.
 }
