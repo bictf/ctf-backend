78e42add15760e62d3c04ae3d9dcac3676c9247b
mango <noamrazbusiness@gmail.com>
2024-07-26 00:26:04 +0300

    Implemented even char number (#56)
    Co-authored-by: MacBook Pro <macbookpro@Noams-MacBook-Pro.local>
    Co-authored-by: rhron255 <142088780+rhron255@users.noreply.github.com>

---
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/EvenAmountOfEveryCharPasswordLevel.kt
@@ -0,0 +1,20 @@
+package biss.ctf.backend.services.passwordlevels
+
+import org.springframework.stereotype.Component
+
+@Component
+class EvenAmountOfEveryCharPasswordLevel : PasswordGameLevel {
+    override fun getLevelDescription(): String {
+        return "Password must have an even amount of every character used."
+    }
+
+    override fun getLevelHint(): String {
+        return "Sometimes Having Lots of Unique Bytes is key."
+    }
+
+    override fun doesAnswerLevel(password: String): Boolean {
+        // Check if every character in the password has an even count
+        val charCounts = password.groupingBy { it }.eachCount()
+        return charCounts.values.all { it % 2 == 0 }
+    }
+}
