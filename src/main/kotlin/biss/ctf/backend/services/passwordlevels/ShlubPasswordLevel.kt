58ef96a872128d92bf9918f1cad7e7d606ce7d86
rhron255 <142088780+rhron255@users.noreply.github.com>
2024-07-26 00:17:35 +0300

    Update ShlubPasswordLevel.kt
    Fixed description to be clearer

--- src/main/kotlin/biss/ctf/backend/services/passwordlevels/ShlubPasswordLevel.kt
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/ShlubPasswordLevel.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component
 @Component
 class ShlubPasswordLevel: PasswordGameLevel {
     override fun getLevelDescription(): String {
-        return "Password must include the neurological value of the word SHLUB"
+        return "Password must include the gematric value of the word SHLUB"
     }
 
     override fun getLevelHint(): String {
