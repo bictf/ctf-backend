34fe5d35b942b2433215cf19126c04be8acc23ab
mango <noamrazbusiness@gmail.com>
2024-07-26 00:13:38 +0300

    Implemented digits sum divisible by 18 (#55)
    Co-authored-by: MacBook Pro <macbookpro@Noams-MacBook-Pro.local>
    Co-authored-by: rhron255 <142088780+rhron255@users.noreply.github.com>

---
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/DigitsSumDivisibleBy18PasswordLevel.kt
@@ -0,0 +1,26 @@
+package biss.ctf.backend.services.passwordlevels
+
+import org.springframework.stereotype.Component
+
+@Component
+class DigitsSumDivisibleBy18PasswordLevel: PasswordGameLevel {
+    companion object {
+        private const val DIVISIBLE_BY = 18
+    }
+    override fun getLevelDescription(): String {
+        return "Sum of digits in password must be divisible by $DIVISIBLE_BY."
+    }
+
+    override fun getLevelHint(): String {
+        return "So simple yet so hard when you're a biss magician..."
+    }
+
+    override fun doesAnswerLevel(password: String): Boolean {
+        // Sum the digits in the password
+        val sumOfDigits = password.filter { it.isDigit() }
+            .map { Character.getNumericValue(it) }
+            .sum()
+        // Check if the sum is divisible by 18
+        return sumOfDigits % DIVISIBLE_BY == 0
+    }
+}
