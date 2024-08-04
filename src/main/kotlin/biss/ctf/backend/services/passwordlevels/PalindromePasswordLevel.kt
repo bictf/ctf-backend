2ec3511234a1db6574f23af141bcdf2e5f79bad5
RedHeadKid111 <54981363+RedHeadKid111@users.noreply.github.com>
2024-08-04 21:21:18 +0300

    43-no-palindromes-in-password (#44)
    Co-authored-by: RoyRoyKing <64307756+RoyRoyKing@users.noreply.github.com>

---
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/PalindromePasswordLevel.kt
@@ -0,0 +1,38 @@
+package biss.ctf.backend.services.passwordlevels
+
+import org.springframework.stereotype.Component
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE)
+class PalindromePasswordLevel: PasswordGameLevel {
+    override fun getLevelDescription(): String {
+        return "Password must not contain any sequence that is a palindrome of length 3 or more."
+    }
+
+    override fun getLevelHint(): String {
+        return "aabb1234"
+    }
+
+    override fun doesAnswerLevel(password: String): Boolean {
+        val length = password.length
+
+        //this is checking all substrings of length 3 and more are palindromes
+        for (start in 0 until (length - 3)) {
+            for (end in (start + 3) until length) {
+                val substring = password.substring(start, end)
+                if (isAlphanumericPalindrome(substring)) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+
+    fun isAlphanumericPalindrome(s: String): Boolean {
+        return s == s.reversed() && !s.matches(Regex("^[a-zA-Z0-9]*$"))
+    }
+}
