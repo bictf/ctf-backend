4c3bf872e4e72bc86977eed574ff916ecc0b9bb7
RedHeadKid <galyurman111@gmail.com>
2024-08-04 19:12:33 +0300

    changed to palindrome of 3 or more

--- src/main/kotlin/biss/ctf/backend/services/passwordlevels/PalindromePasswordLevel.kt
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/PalindromePasswordLevel.kt
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Component
 @Component
 class PalindromePasswordLevel: PasswordGameLevel {
     override fun getLevelDescription(): String {
-        return "Password must not contain any sequence that is a palindrome," +
-                "however, one character may appear twice in a row."
+        return "Password must not contain any sequence that is a palindrome of length 3 or more."
     }
 
     override fun getLevelHint(): String {
@@ -15,20 +14,6 @@ class PalindromePasswordLevel: PasswordGameLevel {
 
     override fun doesAnswerLevel(password: String): Boolean {
         val length = password.length
-        var allowed_repeated_character: String? = null
-
-        // this is checking if there are two repeating characters, and if so are they the allowed character
-        for (start in 0 until (length - 2)) {
-            val substring = password.substring(start, start + 2)
-            if (isPalindrome(substring)) {
-                if (allowed_repeated_character == null) {
-                    allowed_repeated_character = substring.toCharArray()[0].toString()
-                }
-                if (substring != "$allowed_repeated_character$allowed_repeated_character") {
-                    return false
-                }
-            }
-        }
 
         //this is checking all substrings of length 3 and more are palindromes
         for (start in 0 until (length - 3)) {
