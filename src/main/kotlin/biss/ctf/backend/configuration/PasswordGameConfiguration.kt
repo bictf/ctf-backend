d7500d64361b1c86f9f86fd6142f9de292ed2297
rhron255 <ronhasleton@gmail.com>
2024-08-08 23:13:27 +0300

    - Added missing exclusion for tests. - Fixed container properties.

--- src/main/kotlin/biss/ctf/backend/configuration/PasswordGameConfiguration.kt
+++ src/main/kotlin/biss/ctf/backend/configuration/PasswordGameConfiguration.kt
@@ -3,8 +3,10 @@ package biss.ctf.backend.configuration
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.reactive.function.client.WebClient
 
+@Profile("!password-test")
 @Configuration
 class PasswordGameConfiguration {
     @Bean
