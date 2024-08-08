b72877301fb3f315d31c93af803ca3a1b239ba7e
RedHeadKid111 <54981363+RedHeadKid111@users.noreply.github.com>
2024-08-09 00:11:23 +0300

    added leap year level (#77)
    Co-authored-by: RoyRoyKing <roy.safranos@gmail.com>

---
+++ src/main/kotlin/biss/ctf/backend/services/passwordlevels/LeapYearPasswordLevel.kt
@@ -0,0 +1,29 @@
+package biss.ctf.backend.services.passwordlevels
+
+import org.springframework.stereotype.Component
+
+@Component
+class LeapYearPasswordLevel : PasswordGameLevel {
+    override fun getLevelDescription(): String {
+        return "Password must include the a leap year"
+    }
+
+    override fun getLevelHint(): String {
+        return ""
+    }
+
+    override fun doesAnswerLevel(password: String): Boolean {
+        return getNumberSequences(password).any{isLeapYear(it.toInt())}
+    }
+
+    fun getNumberSequences(text: String): List<String> {
+        val getNumberSequencesPattern = "\\d+"
+
+        return Regex(getNumberSequencesPattern).findAll(text).map {it.value}.toList()
+    }
+
+    fun isLeapYear(year: Int): Boolean {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+    }
+}
+
