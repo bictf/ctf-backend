349935bd4fb61ce6e561d7f842c8a0ce08b986ba
rhron255 <142088780+rhron255@users.noreply.github.com>
2024-08-09 18:04:11 +0300

    Update TestConfiguration.kt

--- src/test/kotlin/biss/ctf/backend/TestConfiguration.kt
+++ src/test/kotlin/biss/ctf/backend/TestConfiguration.kt
@@ -11,6 +11,7 @@ import java.nio.file.Path
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.writeText
 
+@Profile("password-test")
 @Configuration
 class TestConfiguration {
     @Bean
