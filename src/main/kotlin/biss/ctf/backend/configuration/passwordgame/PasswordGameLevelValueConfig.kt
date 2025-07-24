1bfb5574b6baa494583b2ae12d60b9472c3aee99
Danko-PC <dankowiki@gmail.com>
2025-07-24 23:40:24 +0300

    105 fix mr + fix routing bug

--- src/main/kotlin/biss/ctf/backend/configuration/passwordgame/PasswordGameLevelValueConfig.kt
+++ src/main/kotlin/biss/ctf/backend/configuration/passwordgame/PasswordGameLevelValueConfig.kt
@@ -1,38 +1,18 @@
 package biss.ctf.backend.configuration.passwordgame
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
 
 @Configuration
-@PropertySource("classpath:password-game.properties")
-@ConfigurationProperties(prefix = "password-game.value")
+@ConfigurationProperties(prefix = "password-game.values")
 class PasswordGameLevelValueConfig {
 
-    lateinit var minimumPasswordLength: String
-    lateinit var bissSpecificWordsList: List<String>
-    lateinit var bissNumber: String
+    var minimumPasswordLength: Int = 0
+    var bissNumber: Int = 0
+    var sumOfAtomicElements: Int = 0
+    var romanNumeralDevisor: Int = 0
+    var staffTotalHeightInCm: Int = 0
     lateinit var bissSponsor: String
-    lateinit var sumOfAtomicElements: String
-    lateinit var romanNumeralDevisor: String
     lateinit var phraseNotToInclude: String
-    lateinit var staffTotalHeightInCm: String
-
-    @Bean
-    fun minimumPasswordLength(): Int = minimumPasswordLength.toInt()
-    @Bean
-    fun bissSpecificWordList(): List<String> = bissSpecificWordsList
-    @Bean
-    fun bissNumber(): Int = bissNumber.toInt()
-    @Bean
-    fun bissSponsor(): String = bissSponsor
-    @Bean
-    fun sumOfAtomicElements(): Int = sumOfAtomicElements.toInt()
-    @Bean
-    fun romanNumeralDevisor(): Int = romanNumeralDevisor.toInt()
-    @Bean
-    fun phraseNotToInclude(): String = phraseNotToInclude
-    @Bean
-    fun staffTotalHeightInCm(): Int = staffTotalHeightInCm.toInt()
+    lateinit var bissSpecificWordsList: List<String>
 }
